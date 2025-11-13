07b2cac16e57dab957ec3a59d0c2f80a01de8bc4
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-13 15:19:45 +0100

    feat:Completato snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,12 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const numsWithFilter = nums.filter(function (num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+});
+console.log(numsWithFilter);
